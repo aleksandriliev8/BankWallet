5fc322c2e5b652f8c52a3128fd72e6bdf6654aa0
Aleksandar Iliev <aleksandriliev8@gmail.com>
2026-02-20 10:10:47 +0200

    Initializing commit

---
+++ wallet-backend/src/test/java/com/bank/wallet/wallet_backend/BankWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.wallet.wallet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
