44005332af298f1b0cab21d7d2e95628b2c9e18a
Aleksandar Iliev <aleksandriliev8@gmail.com>
2026-02-23 07:21:16 +0200

    Major: Added authentication handling system for controled and secured access.

--- wallet-backend/src/main/java/com/bank/wallet/wallet_backend/config/SecurityConfig.java
+++ wallet-backend/src/main/java/com/bank/wallet/wallet_backend/config/SecurityConfig.java
@@ -1,22 +1,26 @@
 package com.bank.wallet.wallet_backend.config;
 
+import com.bank.wallet.wallet_backend.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,13 +32,10 @@ public class SecurityConfig {
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
--- wallet-backend/src/main/java/com/bank/wallet/wallet_backend/repository/UserRepository.java
+++ wallet-backend/src/main/java/com/bank/wallet/wallet_backend/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.bank.wallet.wallet_backend.repository;
 import com.bank.wallet.wallet_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
